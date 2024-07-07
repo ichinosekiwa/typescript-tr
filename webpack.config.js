a04c23ad7ff0f833144be649596d4681068ec425
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2024-07-07 10:29:46 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  entry: {
+    // 何をコンパイルするか
+    bundle: './src/index.ts',
+  },
+  output: {
+    // バンドルしたものをどこに出力するか
+    path: `${__dirname}/dist`,
+    // どんな名前のファイルで出力するか
+    filename: '[name].js',
+  },
+  // 開発モード（ソースマップあり）　or プロダクションモード（圧縮された軽いファイル）
+  mode: 'development',
+  resolve: {
+    // 拡張子を自動で認識させる
+    extensions: ['.ts', '.js'],
+  },
+  devServer: {
+    // ローカルサーバーで何を立ち上げるか
+    static: {
+      directory: `${__dirname}/dist`,
+    },
+    open: true,
+  },
+  module: {
+    // .ts のファイルをts-loaderを使ってコンパイルする
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+};
